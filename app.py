a7848db7c980be139ec71713e667edf534c1a159
Hidayet <h.e.dursun@gmail.com>
2024-02-17 18:53:12 +0300

    Add Gaussian Naive Bayes classifier and preprocess Breast Cancer Wisconsin dataset

--- app.py
+++ app.py
@@ -6,9 +6,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 import time
+import pandas as pd
+import seaborn as sns
 
 class App:
     def __init__(self):
@@ -19,10 +21,12 @@ class App:
         self.params = dict()
         self.clf = None
         self.X, self.y = None, None
+    
     def run(self):
         self.get_dataset()
         self.add_parameter_ui()
         self.generate()
+    
     def Init_Streamlit_Page(self):
         st.title('Streamlit Example')
 
@@ -33,39 +37,66 @@ class App:
 
         self.dataset_name = st.sidebar.selectbox(
             'Select Dataset',
-            ('Iris', 'Breast Cancer', 'Wine')
+            ('Breast Cancer Wisconsin','Iris',)
         )
         st.write(f"## {self.dataset_name} Dataset")
 
         self.classifier_name = st.sidebar.selectbox(
             'Select classifier',
-            ('KNN', 'SVM', 'Random Forest')
+            ('KNN', 'SVM', 'Gaussian Naive Bayes')
         )
     def get_dataset(self):
         data = None
-        if self.dataset_name == 'Iris':
+        if self.dataset_name == 'Breast Cancer Wisconsin':
+            data = self.load_breast_cancer()
+        elif self.dataset_name == 'Iris':
             data = datasets.load_iris()
-        elif self.dataset_name == 'Wine':
-            data = datasets.load_wine()
+            data = pd.DataFrame(data=data.data, columns=data.feature_names)
+            print(type(data))
         else:
-            data = datasets.load_breast_cancer()
-        self.X = data.data
-        self.y = data.target
-        st.write('Shape of dataset:', self.X.shape)
-        st.write('number of classes:', len(np.unique(self.y)))
+            st.write('No dataset selected')
+            
+        st.write('The first 10 rows of the dataset:')
+        st.dataframe(data.head(10))
+        st.write('The last 10 rows of the dataset with irrelevant columns removed:')
+        st.dataframe(data.tail(10))
+        self.preprocess(data)
+    
+    def load_breast_cancer(self):
+        data = pd.read_csv('data.csv')
+        data.drop(columns=['id', 'Unnamed: 32'], axis=1, inplace=True)
+        return data
+    
+    def preprocess(self,df):
+        df['diagnosis'] = df['diagnosis'].map({'M': 1, 'B': 0})
+        self.X = df.drop('diagnosis', axis=1) # Features
+        self.y = df['diagnosis'] # Target variable
+        
+        st.subheader('Correlation Matrix')
+        fig = plt.figure(figsize=(8, 6))
+        sns.heatmap(df.corr(), annot=False, cmap='coolwarm', linewidths=0.5)
+        st.pyplot(fig)
+        
+        st.subheader('Scatter Plot: radius_mean vs texture_mean')
+        malignant_data = df[df['diagnosis'] == 1]  # Extract malignant data
+        benign_data = df[df['diagnosis'] == 0]  # Extract benign data
+        fig2 = plt.figure(figsize=(10, 8))
+        sns.scatterplot(x='radius_mean', y='texture_mean', data=malignant_data, label='Malignant', color='red')
+        sns.scatterplot(x='radius_mean', y='texture_mean', data=benign_data, label='Benign', color='green')
+        plt.title('radius_mean vs texture_mean')
+        plt.legend()
+        st.pyplot(fig2)
 
     def add_parameter_ui(self):
         if self.classifier_name == 'SVM':
-            C = st.sidebar.slider('C', 0.01, 15.0)
+            C = st.sidebar.slider('C', 0.01, 25.0)
             self.params['C'] = C
         elif self.classifier_name == 'KNN':
-            K = st.sidebar.slider('K', 1, 15)
+            K = st.sidebar.slider('K', 1, 25)
             self.params['K'] = K
         else:
-            max_depth = st.sidebar.slider('max_depth', 2, 15)
-            self.params['max_depth'] = max_depth
-            n_estimators = st.sidebar.slider('n_estimators', 1, 100)
-            self.params['n_estimators'] = n_estimators
+            pass
+
 
     def get_classifier(self):
         if self.classifier_name == 'SVM':
@@ -73,25 +104,12 @@ class App:
         elif self.classifier_name == 'KNN':
             self.clf  = KNeighborsClassifier(n_neighbors=self.params['K'])
         else:
-            self.clf  = RandomForestClassifier(n_estimators=self.params['n_estimators'],
-                max_depth=self.params['max_depth'], random_state=1234)
-
-    def create_sinusoid(self):
-        fig = plt.figure()
-        Fs = 8000
-        f = 5
-        sample = 8000
-        x = np.arange(sample)
-        y = np.sin(2 * np.pi * f * x / Fs)
-        plt.plot(x, y)
-        plt.xlabel('sample(n)')
-        plt.ylabel('voltage(V)')
-        return fig
+            self.clf  = GaussianNB()
 
     def generate(self):
         self.get_classifier()
         #### CLASSIFICATION ####
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=1234)
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
 
         self.clf.fit(X_train, y_train)
         y_pred = self.clf.predict(X_test)
